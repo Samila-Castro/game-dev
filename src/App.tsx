cedabf0551f3df7023cc0087cb2e81baf1e8c357
Samila Vieira <samilav1997@gmail.com>
2023-05-17 23:28:14 -0300

    Adicionando passagem de prop

--- src/App.tsx
+++ src/App.tsx
@@ -5,15 +5,25 @@ import { Game } from "./components/Game/Game";
 import { ReactNode, useState } from "react";
 
 function App() {
-  const [currentComponent] = useState<ReactNode>("Game");
+  const [currentComponent, SetCurrentComponent] = useState<ReactNode>("Home");
 
-  const components = () => {
-    if (currentComponent === "Home") return <Home />;
-    if (currentComponent === "Game") return <Game />;
-    return <Home />;
+  const handleContentChange = (value: string) => {
+    SetCurrentComponent(value);
   };
 
-  return <Layout>{components()}</Layout>;
+  const components = () => {
+    if (currentComponent === "Home")
+      return <Home handleContentChange={handleContentChange} />;
+    if (currentComponent === "Game")
+      return <Game handleContentChange={handleContentChange} />;
+    return <Home handleContentChange={handleContentChange} />;
+  };
+
+  return (
+    <>
+      <Layout handleContentChange={handleContentChange}>{components()}</Layout>
+    </>
+  );
 }
 
 export default App;
