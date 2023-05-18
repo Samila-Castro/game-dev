ff2fdbc5afff27f9668db255055f16e56d5c0a2a
Samila Vieira <samilav1997@gmail.com>
2023-05-17 23:28:49 -0300

    Adicionando dark theme provider

--- src/main.tsx
+++ src/main.tsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-
-import "./index.css";
+import "rsuite/dist/rsuite.min.css";
+import { CustomProvider } from "rsuite";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <CustomProvider theme="dark">
+      <App />
+    </CustomProvider>
   </React.StrictMode>
 );
