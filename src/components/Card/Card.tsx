ae6a76917b4197ea49afcd78722d3bc10ceb8f23
Samila Vieira <samilav1997@gmail.com>
2023-05-17 23:25:49 -0300

    Criando component card

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,19 +1,33 @@
 import styles from "./Card.module.css";
 
-export const Card = () => (
-  <div className={styles.cardList}>
-    <article className={styles.card}>
-      <figure className={styles.cardIimage}>
-        <img
-          src="https://images.unsplash.com/photo-1494253109108-2e30c049369b?crop=entropy&cs=srgb&fm=jpg&ixid=MnwxNDU4OXwwfDF8cmFuZG9tfHx8fHx8fHx8MTYyNDcwMTUwOQ&ixlib=rb-1.2.1&q=85"
-          alt="An orange painted blue, cut in half laying on a blue background"
-        />
-      </figure>
-      <div className={styles.cardHeader}>
-        <h4>When life gives you oranges</h4>
-        <p>Lorem ipsun nerd tu altos delerium man</p>
-      </div>
-      <div className={styles.cardFooter}></div>
-    </article>
-  </div>
-);
+interface CardProps {
+  title: string;
+  description: string;
+  image_source?: string;
+  onClick: () => void;
+}
+
+export const Card = ({
+  title,
+  description,
+  onClick,
+  image_source,
+}: CardProps) => {
+  return (
+    <div className={styles.cardList} onClick={onClick}>
+      <article className={styles.card}>
+        <figure className={styles.cardIimage}>
+          <img
+            src={image_source}
+            alt="An orange painted blue, cut in half laying on a blue background"
+          />
+        </figure>
+        <div className={styles.cardHeader}>
+          <h5>{title}</h5>
+          <p>{description}</p>
+        </div>
+        <div className={styles.cardFooter}></div>
+      </article>
+    </div>
+  );
+};
